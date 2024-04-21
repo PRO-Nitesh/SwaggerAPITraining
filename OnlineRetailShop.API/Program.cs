2ebc071350dae5efc522237f1a50c0258bbf45a7
Nitesh Kumar <niteshkumar.rv@theproindia.com>
2024-04-21 17:28:38 +0530

    JWT configuration and updating program.cs

--- OnlineRetailShop.API/Program.cs
+++ OnlineRetailShop.API/Program.cs
@@ -6,19 +6,19 @@ using OnlineRetailShop.Repository.RImplementations;
 using OnlineRetailShop.Services.Interface;
 using OnlineRetailShop.Services.SImplementations;
 using Stripe;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
 
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 
-//adding the ApplicationDbContext
+//adding the ApplicationDbContext , connection string 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("OnlineShopCS")));
-
-
-// Add services to the container.
-
 
 #region CORS
 builder.Services.AddCors(options =>
@@ -33,10 +33,55 @@ builder.Services.AddCors(options =>
 });
 #endregion
 
+#region Jwt configuration 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+#endregion
+
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    c =>
+    {
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "JWT_Authentication", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Jwt Authorization",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+    {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            }
+        },
+        new string[]{}
+    }
+});
+});
 
 
 #region adding services
@@ -60,18 +105,25 @@ builder.Services.AddDbContext<ApplicationDbContext>();
 //builder.Services.AddScoped<IOrderService, OrderService>();
 #endregion
 
-var app = builder.Build();
 
+//adding cache service
+builder.Services.AddMemoryCache();
+
+
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "JWT_Authentication_Authorization v1"));
 }
+
 
 app.UseCors("AllowAnyOriginPolicy");
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization(); 
 
